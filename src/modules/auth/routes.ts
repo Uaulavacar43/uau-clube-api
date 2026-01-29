d7f551c84b84ec8325c2f3d013f267a695048304
josan johnata <jpontes@questrade.com>
2026-01-28 21:39:48 -0300

    feat: add password reset functionality to authentication routes

--- src/modules/auth/routes.ts
+++ src/modules/auth/routes.ts
@@ -1,11 +1,20 @@
 import { Router } from "express";
 import { authMiddleware } from "../../middlewares/auth.middleware";
 import validateRoutePayload from "../../middlewares/validateRoutePayload";
+import { MailingQueue } from "../../queues/MailingQueue";
+import { PrismaUserRepository } from "../../repositories/implementations/PrismaUserRepository";
+import { UserProfileController } from "../userProfile/UserProfileController";
+import { UserProfileService } from "../userProfile/UserProfileService";
 import { LoginUserDTO } from "./dto/LoginUserDTO";
 import { RegisterUserDTO } from "./dto/RegisterUserDTO";
 import { authController } from "./index";
 
 const router = Router();
+
+const userRepository = new PrismaUserRepository();
+const mailingQueue = new MailingQueue();
+const userProfileService = new UserProfileService(userRepository, mailingQueue);
+const userProfileController = new UserProfileController(userProfileService);
 
 router.post(
 	"/register",
@@ -22,6 +31,14 @@ router.get("/firebase-token/:id", authMiddleware, (req, res, next) =>
 
 router.post("/refresh-token", (req, res, next) =>
 	authController.refreshToken(req, res, next),
+);
+
+router.post("/forgot-password", (req, res, next) =>
+	userProfileController.requestPasswordReset(req, res, next),
+);
+
+router.post("/reset-password", (req, res, next) =>
+	userProfileController.resetPassword(req, res, next),
 );
 
 export default router;
